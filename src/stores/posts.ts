3a65322bebc2db2889b3acd2be199fffba1c32d3
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-08-03 08:05:54 +0300

    Добавление кастомного стора

---
+++ src/stores/posts.ts
@@ -0,0 +1,29 @@
+import { reactive, readonly } from 'vue';
+
+interface PostState {
+  foo: string;
+}
+
+export class PostsStore {
+  #state: PostState;
+
+  constructor() {
+    this.#state = reactive<PostState>({
+      foo: 'foo',
+    });
+  }
+
+  getState() {
+    return readonly(this.#state);
+  }
+
+  updateFoo(foo: string) {
+    this.#state.foo = foo;
+  }
+}
+
+const store = new PostsStore();
+
+export function usePosts() {
+  return store;
+}
