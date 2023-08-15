eb87cf6e1317df4045cb280f365c62a394c85fb6
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-08-15 21:30:12 +0300

    Добавление компонента редактирования поста

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { TimeLinePost } from '../posts';
+
+defineProps<{ post: TimeLinePost }>();
+</script>
+
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">Post Title</div>
+        <input
+          type="text"
+          class="input"
+        />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,17 @@
-<template>new Post</template>
+<script setup lang="ts">
+import { DateTime } from 'luxon';
+import PostWriter from '../components/PostWriter.vue';
+import { TimeLinePost } from '../posts';
+
+const post: TimeLinePost = {
+  id: '-1',
+  title: 'Title',
+  created: DateTime.now(),
+};
+</script>
+
+<template>
+  new Post
+
+  <PostWriter :post="post" />
+</template>
