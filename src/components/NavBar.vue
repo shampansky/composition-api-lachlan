d05a50279f7b2b1b2b4c5496d910ec839b7d755f
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-08-15 21:14:18 +0300

    Добавление компонента нового поста

---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="navbar">
+    <div class="navbar-end">
+      <div class="buttons">
+        <RouterLink
+          to="/posts/new"
+          class="button"
+        >
+          New Post
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
