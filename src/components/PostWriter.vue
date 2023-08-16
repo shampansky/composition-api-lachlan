064148bd79190724c38b9dac741ee223e60b7c66
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-08-16 18:31:37 +0300

    Добавление блока редактирования поста и превью

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { TimeLinePost } from '../posts';
 
 const props = defineProps<{ post: TimeLinePost }>();
 const title = ref(props.post.title);
+const content = ref(props.post.markdown);
+const contentEditable = ref<HTMLDivElement>();
+
+onMounted(() => {
+  if (!contentEditable.value) {
+    throw Error('ContentEditalbe DOM node was not found');
+  }
+  contentEditable.value.innerText = content.value;
+});
+
+function handleInput() {
+  if (!contentEditable.value) {
+    throw Error('ContentEditalbe DOM node was not found');
+  }
+  content.value = contentEditable.value?.innerText;
+}
 </script>
 
 <template>
@@ -17,6 +33,19 @@ const title = ref(props.post.title);
           v-model="title"
         />
       </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div
+        contenteditable="true"
+        ref="contentEditable"
+        @input="handleInput"
+      />
+    </div>
+    <div class="column">
+      {{ content }}
     </div>
   </div>
 </template>
