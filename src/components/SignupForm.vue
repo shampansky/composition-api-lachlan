8454d4fea4136b258bcc9952d7c82ad247ff4481
Evgeniy Shadrin <e.shadrin@aristos.pw>
2023-09-01 17:30:43 +0300

    Создание компонента обертки для формы

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,74 +1,18 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import FormInput from './FormInput.vue';
-import { NewUser } from '../users';
-import { validate, length, required } from '../validation';
-import { useUsers } from '../stores/users';
 import { useModal } from '../composables/modal';
-
-const username = ref('');
-const usernameInput = ref<InstanceType<typeof FormInput> | null>(null);
-const usernameStatus = computed(() => {
-  return validate(username.value, [required, length({ min: 5, max: 10 })]);
-});
-
-const password = ref('');
-const passwordInput = ref<InstanceType<typeof FormInput> | null>(null);
-const passwordStatus = computed(() => {
-  return validate(password.value, [required, length({ min: 10, max: 40 })]);
-});
-
-const isInvalid = computed(() => {
-  return !usernameStatus.value.valid || !passwordStatus.value.valid;
-});
+import { useUsers } from '../stores/users';
+import { NewUser } from '../users';
+import UserForm from './UserForm.vue';
 
 const usersStore = useUsers();
 const modal = useModal();
 
-async function handleSubmit() {
-  usernameInput.value?.validate();
-  passwordInput.value?.validate();
-  if (isInvalid.value) return;
-  const newUser: NewUser = {
-    username: username.value,
-    password: password.value,
-  };
-
-  try {
-    await usersStore.createUser(newUser);
-  } catch (e) {}
-
+async function handleSignup(newUser: NewUser) {
+  await usersStore.createUser(newUser);
   modal.hideModal();
 }
 </script>
 
 <template>
-  <form
-    class="form"
-    @submit.prevent="handleSubmit"
-  >
-    <FormInput
-      ref="usernameInput"
-      name="Username"
-      v-model="username"
-      :status="usernameStatus"
-      type="text"
-    />
-    <FormInput
-      ref="passwordInput"
-      name="Password"
-      v-model="password"
-      :status="passwordStatus"
-      type="password"
-    />
-    <button class="button">Submit</button>
-  </form>
+  <UserForm @submit="handleSignup" />
 </template>
-
-<style scoped>
-.form {
-  background-color: #fff;
-  padding: 30px;
-  margin-top: 50px;
-}
-</style>
---
+++ src/components/UserForm.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import FormInput from './FormInput.vue';
+import { NewUser } from '../users';
+import { validate, length, required } from '../validation';
+
+const emit = defineEmits<{
+  (event: 'submit', payload: NewUser): void;
+}>();
+
+const username = ref('');
+const usernameInput = ref<InstanceType<typeof FormInput> | null>(null);
+const usernameStatus = computed(() => {
+  return validate(username.value, [required, length({ min: 5, max: 10 })]);
+});
+
+const password = ref('');
+const passwordInput = ref<InstanceType<typeof FormInput> | null>(null);
+const passwordStatus = computed(() => {
+  return validate(password.value, [required, length({ min: 10, max: 40 })]);
+});
+
+const isInvalid = computed(() => {
+  return !usernameStatus.value.valid || !passwordStatus.value.valid;
+});
+
+async function handleSubmit() {
+  usernameInput.value?.validate();
+  passwordInput.value?.validate();
+  if (isInvalid.value) return;
+  const newUser: NewUser = {
+    username: username.value,
+    password: password.value,
+  };
+
+  try {
+    emit('submit', newUser);
+  } catch (e) {}
+}
+</script>
+
+<template>
+  <form
+    class="form"
+    @submit.prevent="handleSubmit"
+  >
+    <FormInput
+      ref="usernameInput"
+      name="Username"
+      v-model="username"
+      :status="usernameStatus"
+      type="text"
+    />
+    <FormInput
+      ref="passwordInput"
+      name="Password"
+      v-model="password"
+      :status="passwordStatus"
+      type="password"
+    />
+    <button class="button">Submit</button>
+  </form>
+</template>
+
+<style scoped>
+.form {
+  background-color: #fff;
+  padding: 30px;
+  margin-top: 50px;
+}
+</style>
